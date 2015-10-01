4fd2b71b447cb6d847e15fe835cf16bd96cb4667
Jorge Baier <jabaier@gmail.com>
2015-10-01 15:56:31 -0300

    encriptacion rot y suma de listas

--- strings_listas_ej0.py
+++ strings_listas_ej0.py
@@ -18,5 +18,48 @@ def sumalista_cool(l):
         suma = suma + e
     return suma
 
-print(sumalista_cool([1,1,1,1]))
-print(sumalista_cool([1,5,3,7,11]))
+
+def desafio_google(l):
+    i = 0
+    while i < len(l):
+        if sumalista(l[:i])==sumalista(l[i+1:]):
+            return i
+        i = i + 1
+    return -1
+
+
+def mayusculas():
+    i = ord('A')
+    limite = ord('Z')
+    may=''
+    while i <= limite:
+        may = may + chr(i)
+        i = i + 1
+    return may
+
+def minusculas():
+    return mayusculas().lower()
+
+def encriptar_rot(mensaje, incremento):
+    M = mayusculas()
+    m = minusculas()
+
+    respuesta = ''
+    for c in mensaje:
+        indiceM = M.find(c)
+        indicem = m.find(c)
+
+        if indiceM > -1:
+            respuesta = respuesta + M[(indiceM+incremento)%26]
+        elif indicem > -1:
+            respuesta = respuesta + m[(indicem+incremento)%26]
+        else:
+            respuesta = respuesta + c
+
+    return respuesta
+
+men = "Andate al cerro que mas te guste, querido"
+enc = encriptar_rot(men,13)
+desenc = encriptar_rot(enc,13)
+print(enc)
+print(desenc)
